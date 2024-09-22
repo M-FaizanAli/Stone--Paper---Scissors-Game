597bd283cb7f2088593542e290863cff5c1b1954
ukaleem <mrfaizan726@gmail.com>
2024-09-22 22:59:58 +0500

    Refactor game UI layout and functionality

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Here");
+const choices = document.querySelectorAll(".choose");
+ for(const choice of choices){
+    console.log(choice);
+    choice.addEventListener("click", () => {
+        const Selected = choice.getAttribute("id");
+        console.log(Selected);
+    })
+}
+
