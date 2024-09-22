ca8a9c4313e2664e3ccc7fa8921f8cd83a47bde1
ukaleem <mrfaizan726@gmail.com>
2024-09-22 23:29:06 +0500

    Refactor game UI layout and functionality, and add game logic for user and computer choices

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
+function playGame(userChoice){
+    console.log(userChoice);
+    let compChoice = comSelection();
+    if(compChoice === userChoice){
+        console.log("It's a tie!");
+    }
+};
+
+// Getting user Selection
 const choices = document.querySelectorAll(".choose");
- for(const choice of choices){
-    console.log(choice);
+for(const choice of choices){
     choice.addEventListener("click", () => {
-        const Selected = choice.getAttribute("id");
-        console.log(Selected);
+        const selected = choice.getAttribute("id");
+        playGame(selected);
     })
 }
+
+const compChoice = ["rock", "paper", "scissors"];
+
+// Getting Computer Selection
+function comSelection(){
+    let comp = compChoice[randomNum()];
+    return comp;
+}
+
+function randomNum(){
+    let rand = Math.floor(Math.random() * 3);
+    return (rand);
+}
+
+
 
